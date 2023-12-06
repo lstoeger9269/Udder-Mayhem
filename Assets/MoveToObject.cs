70cf00a532bcf5be98588bf45970cdfbe1c810c6
Hugo berneis <hberneis9830@student.pps.net>
2023-12-06 11:56:49 -0800

    boss chases character + ceiling fan that spins + fixed health bar error

---
+++ Assets/MoveToObject.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveToObject : MonoBehaviour
+{
+    public GameObject FollowingObject;
+    public GameObject OtherObject;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        FollowingObject.transform.position = Vector3.MoveTowards(FollowingObject.transform.position, OtherObject.transform.position, speed);
+    }
+}
