efcff202ac65253402cc4d0bbf30a03d4c84e8ff
HugoBerneis <hberneis9830@student.pps.net>
2024-05-20 11:38:42 -0700

    https://trello.com/c/jEbzsllc/51-tutorial-bird

--- Assets/BirdScript.cs
+++ Assets/BirdScript.cs
@@ -4,11 +4,29 @@ using UnityEngine;
 
 public class BirdScript : MonoBehaviour
 {
+    Animator anim;
     public bool talking = false;
     public bool flying = true;
+    private IEnumerator coroutine;
+
+    public GameObject textbox1;
+    public GameObject textbox2;
+    public GameObject textbox3;
+    public bool text1 = false;
+    public bool text2 = false;
+    public bool text3 = false;
+    public bool bugbool = true;
     // Start is called before the first frame update
     void Start()
     {
+        anim=gameObject.GetComponent<Animator>();
+
+        coroutine = BirdTalk(4.0f);
+        StartCoroutine(coroutine);
+
+        textbox1.SetActive(false);
+        textbox2.SetActive(false);
+        textbox3.SetActive(false);
 
     }
 
@@ -17,23 +35,61 @@ public class BirdScript : MonoBehaviour
     {
         if(!talking && !flying){
             anim.SetBool("birdLeave", true);
+            anim.SetBool("birdTalk", false);
             
         }
+        if((Input.GetKeyDown(KeyCode.M))){
+            if(text1){
+                textbox1.SetActive(false);
+                text1 = false;
+                textbox2.SetActive(true);
+                text2 = true;
+                Debug.Log("text");
+            }
+            else if(text2){
+                textbox2.SetActive(false);
+                text2 = false;
+                textbox3.SetActive(true);
+                text3 = true;
+                Debug.Log("text");
+            }
+            else if(text3){
+                textbox3.SetActive(false);
+                text3 = false;
+                anim.SetBool("birdLeave", true);
+                coroutine = BirdDie(3.0f);
+                StartCoroutine(coroutine);
+                gameObject.GetComponent<TruckDriverSpawn>().Spawn();
+            }
+        }
     }
-    private void OnTriggerEnter(Collider collider)
-      {
-          if (collider.CompareTag("Road"))
-          {
-            anim.SetBool("birdTalk", true);
-            talking = true;
-            flying = false;
-          }
-      }
     
-    public void NotTalking()
-    {
-        talking = false;
-
+    public void Talking(){
+        talking = true;
+        anim.SetBool("birdTalk", true);
+        if(bugbool){
+        textbox1.SetActive(true);
+        text1 = true;
+        bugbool = false;
+        Debug.Log("text");
+        }
     }
-
+    private IEnumerator BirdTalk(float waitTime)
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(waitTime);
+            Talking();
+        }
+    }
+    private IEnumerator BirdDie(float waitTime)
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(waitTime);
+            anim.SetBool("birdTalk", false);
+            anim.SetBool("birdLeave", false);
+            anim.SetBool("birdDie", true);
+        }
+    }
 }
